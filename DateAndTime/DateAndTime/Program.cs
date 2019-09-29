1390be1171a4f815c29ad52d595c131c5fadaf1a
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-29 10:54:49 +0300

    Created two small projects one with DateTime and the other using readline and int parse

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace DateAndTime
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            Console.WriteLine(dateTime.ToString("yy/MMM/dd hh:mm"));
+        }
+    }
+}
---
+++ PrintAge/PrintAge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintAge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your Age: ");
+            int age;
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+        }
+    }
+}
