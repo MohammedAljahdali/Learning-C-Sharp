e2a1f9a22bf63b7be9047fa00d2cade56edb599c
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-10-04 04:05:06 +0300

    Try and Catch test app

---
+++ TryCatchTest/TryCatchTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TryCatchTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter two numbers to divide: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondtNumber = int.Parse(Console.ReadLine());
+            try
+            {
+                int output = firstNumber / secondtNumber;
+                Console.WriteLine("The out put is: " + output);
+            } catch (Exception x)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
